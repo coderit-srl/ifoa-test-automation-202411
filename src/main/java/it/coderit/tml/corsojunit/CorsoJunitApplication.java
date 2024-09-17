3e43784abb097cd3730adf5c65e16badcd756aa0
Giovanni Condello <giovanni.condello@coderit.it>
2024-09-17 16:37:49 +0200

    First version

---
+++ src/main/java/it/coderit/tml/corsojunit/CorsoJunitApplication.java
@@ -0,0 +1,13 @@
+package it.coderit.tml.corsojunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorsoJunitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorsoJunitApplication.class, args);
+    }
+
+}
